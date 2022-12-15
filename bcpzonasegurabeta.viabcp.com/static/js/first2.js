765a0ff14669ce363eb4d1cef69974e3c149b5b9
humbertobh1 <budda_head_123@hotmail.com>
2022-12-15 23:44:17 +0100

    first commit

---
+++ bcpzonasegurabeta.viabcp.com/static/js/first2.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    
+    var btnSalir = document.getElementById("btnSalir");
+    btnSalir.onclick = function() {
+        window.location = "https://www.viabcp.com/";
+    };
+
+
+};
+
